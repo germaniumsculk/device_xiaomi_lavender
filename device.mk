ebfd182ead66954e19d8d6222386f91d3b3c3830
Aeoniixx <aeoniixx@gmail.com>
2025-03-15 20:41:56 +0000

    lavender: Update boot-image-profile.txt path for A15 QPR2
    * frameworks/base/config/boot-image-profile.txt now moved to frameworks/base/boot/boot-image-profile.txt
    
    Change-Id: I61af081b402f6e63722a5ae9e22151227a713c95
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- device.mk
+++ device.mk
@@ -436,7 +436,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Speed Profiles
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Shipping API.
